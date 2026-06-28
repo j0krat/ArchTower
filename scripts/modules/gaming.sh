9751228d57a023ec580571bb40824d3eef32476c
j0krat <joaquin.vejar.261105@gmail.com>
2026-06-28 16:04:52 -0400

    feat: add gaming and hybrid package profiles

---
+++ scripts/modules/gaming.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+gaming_packages() {
+    echo "steam gamemode lib32-gamemode mangohud lib32-mangohud vulkan-tools wine winetricks"
+}
---
+++ scripts/modules/hybrid.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+hybrid_packages() {
+    echo "$(study_packages) $(gaming_packages)"
+}
